58712f2ef3e3219c21fd867d2a3cca856565204d
Nishant-l <nishant3151998@gmail.com>
2021-07-16 08:13:01 +0530

    🏃:Fettching data from API using ES6 fetch API;

--- index.js
+++ index.js
@@ -9,21 +9,15 @@ const nameEle=document.querySelector('#name');
 //adding event listners 
 button.addEventListener('click',get)
 
-// defining functions on event listners
+
 function get(){
-    const xhrRequest=new XMLHttpRequest();
-    xhrRequest.onload=function(){
-        data=JSON.parse(xhrRequest.response);
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res=>res.json())
+    .then((data)=>{
         data=data.meals[0];
         imgEle.setAttribute('src',data.strMealThumb);
         nameEle.textContent=data.strMeal;
-        // vedEle.setAttribute('src',data.strYoutube);
-        console.log(data.strYoutube);
-
-    }
-
-    xhrRequest.open('get','https://www.themealdb.com/api/json/v1/1/random.php');
-    xhrRequest.send();
+        }
+    )
 }
 
-console.log(data,'###############');
