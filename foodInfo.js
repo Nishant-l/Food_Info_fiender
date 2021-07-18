d8dc31f9ff9a232fb1dfc70849895c13461c06d1
Nishant-l <nishant3151998@gmail.com>
2021-07-18 11:55:44 +0530

    🎄:adding style to food info page

--- foodInfo.js
+++ foodInfo.js
@@ -2,6 +2,7 @@
 (function get(id){
     const imgEle=document.querySelector('img');
     const nameEle=document.querySelector('#name');
+    const info=document.querySelector('#info');
     fetch(`https:www.themealdb.com/api/json/v1/1/lookup.php?i=${localStorage.id}`)
     .then((res)=>{
         // location.href='./index2.html'
@@ -11,18 +12,18 @@
         data=data.meals[0];
         imgEle.setAttribute('src',data.strMealThumb);
         nameEle.textContent=data.strMeal;
+        info.textContent=data.strInstructions;
     })
-    // .then(()=>{
-    //     const listt=searchBar.querySelector('#listt');
-    //         if(listt!=null)
-    //             listt.remove();
-    // })
 })();
 
 // cashing the dom
 const homePage=document.querySelector('#HomePage');
+const fevPageButton=document.querySelector('#fevList');
 
 // adding event listners
 homePage.addEventListener('click',()=>{
     location.href='./index.html';
+})
+fevPageButton.addEventListener('click',()=>{
+    location.href='./feverate.html';
 })
